5ea88c7e34467a34c8b6850e2b584f1bdc683213
m_m311 <m.mohammadzadeh3111@gmail.com>
2023-10-19 22:08:23 +0330

    connecting to openWeather API

--- website/views.py
+++ website/views.py
@@ -1,4 +1,10 @@
+import requests
 from django.shortcuts import render
 
 def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=32e4d2a6840e4e530bb3e477aec0c38b'
+    city= 'Tehran'
+
+    r= requests.get(url.format(city)).json()
+    #print(r)
     return render(request, 'template.html')
