165ea9629fbb2201d1c282d0b7b5920745789626
m_m311 <m.mohammadzadeh3111@gmail.com>
2023-10-23 22:31:48 +0330

    almost finished the app

--- website/views.py
+++ website/views.py
@@ -1,20 +1,27 @@
 import requests
 from django.shortcuts import render
+from .models import City
+from .forms import CityForm
 
 def index(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=32e4d2a6840e4e530bb3e477aec0c38b'
-    city= 'Tehran'
+     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=32e4d2a6840e4e530bb3e477aec0c38b'
+     if request.method == 'POST':
+        CityForm(request.POST).save()
 
-    r= requests.get(url.format(city)).json()
-    #print(r.text)
-    
-    city_weather= {
-        'city': city,
-        'temp': r['main']['temp'],
-        'desc': r['weather'][0]['description'],
-        'icon': r['weather'][0]['icon'],
-    }
+     form = CityForm()
+     cities = City.objects.all()
+     data = []
 
-    print(city_weather)
+     for city in cities:
+        r= requests.get(url.format(city)).json()
+        city_weather= {
+            'city': city.name,
+            'temp': r['main']['temp'],
+            'desc': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon'],
+        }
 
-    return render(request, 'template.html', {'city_weather': city_weather})
+        data.append(city_weather)
+        print(data)
+
+     return render(request, 'template.html', {'data': data, 'form': form})
